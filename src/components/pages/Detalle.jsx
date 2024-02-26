614403bd50a72c542327110462f72272ca1a5a6f
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-26 18:08:22 -0300

    se muestra el producto seleccionado en detalle

--- src/components/pages/Detalle.jsx
+++ src/components/pages/Detalle.jsx
@@ -31,17 +31,12 @@ const Detalle = () => {
   return (
     <Row className="fila my-5">
       <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6} className="col ">
-        <Image src={cafe} alt="cafe" fluid id="imgDetalle"></Image>
+        <Image src={producto.imagen} alt={producto.nombreProducto} fluid id="imgDetalle"></Image>
       </Col>
       <Col xs={12} sm={12} md={6} lg={6} xl={6} xxl={6} className="col ">
-        <h3 className="m-3">Cafe Americano</h3>
-        <p className="m-3">
-          El cafe americano es una bebida caliente que consiste en un espresso
-          diluido con agua caliente lo que resulta en una taza de cafe suave y
-          aromatico. Es una opcion popular para aquellos que prefieren un cafe
-          menos intenso que el espresso tradicional. Perfecto para disfrutar en
-          cualquier momento del dia.
-        </p>
+        <h3 className="m-3">{producto.nombreProducto}</h3>
+        <p className="m-3"> {producto.descripcion_amplia}
+          </p>
       </Col>
     </Row>
   );
