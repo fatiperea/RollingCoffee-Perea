76792805698b6e4146b5cc63aae7d043df0b4b40
fatiperea <fatimaperea3pppp0@gmail.com>
2024-02-26 16:46:37 -0300

    agrego vlidaciones de login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+
+const {
+  register,
+  handleSubmit,
+  formState: { errors },
+} = useForm();
+
+const onSubmit = (data) => {
+  console.log(data);
+};
 
 const Login = () => {
   return (
@@ -7,14 +18,20 @@ const Login = () => {
       <Form>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Ingrese email" />
+          <Form.Control type="email" placeholder="Ingrese email"
+          {...register("email", {
+            required: "El nombre de usuario es obligatorio",
+          })} />
           <Form.Text className="text-muted">
           </Form.Text>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Contraseña</Form.Label>
-          <Form.Control type="password" placeholder="Contraseña" />
+          <Form.Control type="password" placeholder="Contraseña" 
+          {...register("password", {
+            required: "La contraseña es obligatoria",
+          })}/>
         </Form.Group>
         <Button variant="primary" type="submit">
           Enviar
